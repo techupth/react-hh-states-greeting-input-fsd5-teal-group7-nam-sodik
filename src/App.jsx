74c7dc73533d9578f7c0523ffb5a63ba11fa9d22
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2023-12-21 13:20:35 +0700

    add input message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [message, setMessage] = useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => {
+            setInputText(event.target.value);
+          }}
+          value={inputText}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setMessage(inputText === "" ? "Please Enter Text" : inputText);
+          }}
+        >
+          Update text
+        </button>
+
+        <button
+          onClick={() => {
+            setMessage("Greeting Message");
+          }}
+        >
+          Reset text
+        </button>
       </div>
     </div>
   );
