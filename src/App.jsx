7089d59da5bbb414c9e5ee560dc70f1f4380cf06
Sodikcp <paramesh.rah@gmail.com>
2023-12-21 12:13:51 +0700

    feat:complete Ex2 state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 function App() {
+  const [greetingInputText, setGreetingInputText] = useState("");
+  const [newGreetingMessage, setNewGreetingMessage] =
+    useState("Greeting Message");
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{newGreetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => {
+            setGreetingInputText(event.target.value);
+          }}
+          value={greetingInputText}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setNewGreetingMessage(greetingInputText);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
